57e26e760b914caf423431019ac06f7d87c3d48e
Gary Belvin <gdb@google.com>
2020-06-15 13:12:04 +0100

    bits2octets

--- go/vrf/conversion.go
+++ go/vrf/conversion.go
@@ -100,6 +100,23 @@ func int2octets(x *big.Int, rlen int) []byte {
 	return b
 }
 
+//  bits2octets takes as input a sequence of blen bits and outputs a sequence of rlen bits.
+//
+// https://datatracker.ietf.org/doc/html/rfc6979#section-2.3.4
+func bits2octets(b []byte, rlen int, q *big.Int) []byte {
+	//  1.  The input sequence b is converted into an integer value z1 through the bits2int transform:
+	z1 := bits2int(b, q.BitLen())
+	//  2.  z1 is reduced modulo q, yielding z2 (an integer between 0 and q-1, inclusive):
+	z2 := new(big.Int).Mod(z1, q)
+
+	// Note that since z1 is less than 2^qlen, that modular reduction
+	// can be implemented with a simple conditional subtraction:
+	// z2 = z1-q if that value is non-negative; otherwise, z2 = z1.
+
+	// 3.  z2 is transformed into a sequence of octets (a sequence of rlen bits) by applying int2octets.
+	return int2octets(z2, rlen)
+}
+
 // SECG1EncodeCompressed converts an EC point to an octet string according to
 // the encoding specified in Section 2.3.3 of [SECG1] with point compression
 // on. This implies ptLen = 2n + 1 = 33.
